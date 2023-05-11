8ba881568327af18d96980ec1880e71818c7f88f
Arian <arianahmadi1@gmail.com>
2023-05-11 00:11:00 -0400

    refactor and update

--- fetcher.js
+++ fetcher.js
@@ -1,53 +1,24 @@
-const request = require("request");
-const fs = require("fs");
-const readline = require("readline");
+const request = require('request');
+const fs = require('fs');
 
-const args = process.argv.slice(2);
+let args = process.argv.slice(2);
 
-if (args.length !== 2) {
-  console.log("Usage: node fetcher.js <url> <file path>");
-  process.exit(1);
-}
+request(args[0], (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
 
-const url = args[0];
-const filePath = args[1];
+  fs.writeFile(args[1], body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-if (fs.existsSync(filePath)) {
-  rl.question(
-    `File '${filePath}' already exists. Overwrite? (Y/N) `,
-    (answer) => {
-      if (answer.toUpperCase() !== "Y") {
-        console.log("Exiting...");
-        process.exit(1);
+    fs.stat(args[1], (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-      downloadFile();
-    }
-  );
-} else {
-  downloadFile();
-}
-
-const downloadFile = function() {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log(`Error: ${error.message}`);
-      process.exit(1);
-    }
-    if (response.statusCode !== 200) {
-      console.log(`Error: ${response.statusCode} ${response.statusMessage}`);
-      process.exit(1);
-    }
-    fs.writeFile(filePath, body, (error) => {
-      if (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-      }
-      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+      console.log(`Downloaded and saved ${stats.size} bytes to ${args[1]}`);
     });
   });
-};
+});
